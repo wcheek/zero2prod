f4a4a4792997063daebdeed574c38742919e9209
Wesley Jon Cheek <w-cheek@satake-japan.co.jp>
2025-02-28 17:04:11 +0900

    Okay I guess I'm learning...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use sqlx::PgPool;
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 use zero2prod::configuration;
 use zero2prod::startup;
@@ -5,7 +7,10 @@ use zero2prod::startup;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    startup::run(listener)?.await
+    startup::run(listener, connection_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,7 @@
 use actix_web::{HttpResponse, Responder, post, web};
+use chrono::Utc;
+use sqlx::{PgConnection, PgPool};
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 struct EmailSubscriptionFormData {
@@ -7,6 +10,29 @@ struct EmailSubscriptionFormData {
 }
 
 #[post("/subscriptions")]
-async fn subscribe(form: web::Form<EmailSubscriptionFormData>) -> impl Responder {
-    HttpResponse::Ok().finish()
+async fn subscribe(
+    form: web::Form<EmailSubscriptionFormData>,
+    pool: web::Data<PgPool>,
+) -> impl Responder {
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    // We use `get_ref` to get an immutable reference to the `PgConnection`
+    // wrapped by `web::Data`.
+    .execute(pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
