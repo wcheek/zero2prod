d8495dd23e80781eb336d84d173c4be74ca83630
Wesley Jon Cheek <w-cheek@satake-japan.co.jp>
2025-03-04 15:45:12 +0900

    Docker setup before networking fix

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,12 @@ async fn main() -> std::io::Result<()> {
     telemetry::init_subscriber(subscriber);
 
     let configuration = configuration::get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string())
+        .expect("Failed to connect to Postgres connection pool.");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
     startup::run(listener, connection_pool)?.await
 }
